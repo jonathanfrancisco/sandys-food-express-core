6bf39693c9ad23335dda2ae66a1956f8087a955e
Jonathan Buted Francisco <jonathanfrancisco999@gmail.com>
2021-05-08 23:35:59 +0800

    Refactor single file migration for users table

--- migrations/Migration20210411142727.ts
+++
@@ -1,9 +0,0 @@
-import { Migration } from '@mikro-orm/migrations';
-
-export class Migration20210411142727 extends Migration {
-  async up(): Promise<void> {
-    this.addSql(
-      'create table "user" ("id" serial primary key, "name" varchar(255) not null, "email" varchar(255) not null, "address" varchar(255) not null, "created_at" timestamptz(0) null, "updated_at" timestamptz(0) null);',
-    );
-  }
-}
